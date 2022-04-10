d54ce453bf581d5c3eff4ee0f9b8e5192ed9bc31
100minus13 <stven3au@gmail.com>
2022-04-10 23:01:51 +0800

    sample python binary search

--- code/dataset/binary_search_nearest_rating.py
+++ code/dataset/binary_search_nearest_rating.py
@@ -1,9 +1,25 @@
+import enum
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("5_letters.csv")
-sort_df = df.sort_values(by=["difficulty"], ascending = True)
-print (sort_df[sort_df["difficulty"] < 0]) 
+def binary_search_nearest(df, rating):
+    # return nearest rating in the csv
+    
+    difficulty = df["difficulty"]
+    left, right = 0, len(difficulty)-1
+    while left < right:
+        mid = (left + right) // 2
+        if rating > difficulty[mid]:
+            left =  mid + 1
+        else:
+            right = mid - 1
+        # print (left, right)
+    return left
+
+if __name__ == '__main__':
+    df = pd.read_csv("./words_lognormal.csv")
+    print(df.iloc[[binary_search_nearest(df, 1300)]])
+
 # print(data.loc[[2494]])
 
 # init function
