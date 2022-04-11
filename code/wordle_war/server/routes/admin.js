8e846b2933efc742cb51172aa711a5c3eaf70399
Ivan <49149560+ivan00105@users.noreply.github.com>
2022-04-11 16:36:17 +0800

    updated admin panel with login
    ac:login
    pw:login

--- code/wordle_war/server/routes/admin.js
+++ code/wordle_war/server/routes/admin.js
@@ -10,8 +10,29 @@ const AdminBroOptions = {
     
     resources: [user],
     rootPath: '/admin',
+    branding: {
+      companyName: 'Avalanche',
+      softwareBrothers: false,   // if Software Brothers logos should be shown in the sidebar footer
+    },
 };
-  const adminBro = new AdminBro(AdminBroOptions);
-  const router = AdminBroExpress.buildRouter(adminBro);
+const ADMIN = {
+  email: 'admin',
+  password: 'admin',
+}
 
+  const adminBro = new AdminBro(AdminBroOptions);
+  //const router = AdminBroExpress.buildRouter(adminBro);
+  const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
+    cookieName: 'adminBro',	
+    cookiePassword: 'testtest',
+    authenticate: async (email, password) => {
+      if (ADMIN.password === password && ADMIN.email === email) {
+        return ADMIN
+      }
+        return null
+      },
+    }, null, { 
+      resave: false,	
+      saveUninitialized: true,	
+  });
 module.exports= router;
